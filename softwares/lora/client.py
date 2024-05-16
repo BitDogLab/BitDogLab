ec3064f27707b9f8fbd70e94fa60ff86788acedd
Pedro Ivo <pedro.guimaraes@ee.ufcg.edu.br>
2024-05-16 15:42:35 -0300

    uploading lora example

---
+++ softwares/lora/client.py
@@ -0,0 +1,27 @@
+from machine import Pin
+from time import sleep
+from ulora import LoRa, ModemConfig, SPIConfig
+
+verde = Pin(11, Pin.OUT) # verde
+azul = Pin(12, Pin.OUT) # azul
+vermelho = Pin(13, Pin.OUT) #vermelho
+
+# Lora Parameters
+RFM95_RST = 28
+RFM95_SPIBUS = SPIConfig.rp2_0
+RFM95_CS = 17
+RFM95_INT = 20
+RF95_FREQ = 868.0
+RF95_POW = 10
+CLIENT_ADDRESS = 1
+SERVER_ADDRESS = 2
+
+# initialise radio
+lora = LoRa(RFM95_SPIBUS, RFM95_INT, CLIENT_ADDRESS, RFM95_CS, reset_pin=RFM95_RST, freq=RF95_FREQ, tx_power=RF95_POW, acks=True)
+
+azul.on()
+# loop and send data
+while True:
+    lora.send_to_wait("This is a test message", SERVER_ADDRESS)
+    print("sent")
+    sleep(10)
---
+++ softwares/lora/server.py
@@ -0,0 +1,36 @@
+from machine import Pin
+from time import sleep
+from ulora import LoRa, ModemConfig, SPIConfig
+
+verde = Pin(11, Pin.OUT) # verde
+azul = Pin(12, Pin.OUT) # azul
+vermelho = Pin(13, Pin.OUT) #vermelho
+
+# This is our callback function that runs when a message is received
+def on_recv(payload):
+    print("From:", payload.header_from)
+    print("Received:", payload.message)
+    print("RSSI: {}; SNR: {}".format(payload.rssi, payload.snr))
+
+# Lora Parameters
+RFM95_RST = 28
+RFM95_SPIBUS = SPIConfig.rp2_0
+RFM95_CS = 17
+RFM95_INT = 20
+RF95_FREQ = 868.0
+RF95_POW = 14
+CLIENT_ADDRESS = 1
+SERVER_ADDRESS = 2
+
+# initialise radio
+lora = LoRa(RFM95_SPIBUS, RFM95_INT, SERVER_ADDRESS, RFM95_CS, reset_pin=RFM95_RST, freq=RF95_FREQ, tx_power=RF95_POW, acks=True)
+
+# set callback
+lora.on_recv = on_recv
+
+# set to listen continuously
+lora.set_mode_rx()
+verde.on()
+# loop and wait for data
+while True:
+    sleep(0.1)
